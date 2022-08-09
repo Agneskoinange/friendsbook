0bee2cf543318d9d20d42bd769ea3545ca2844e3
Agnes <koinageagness@gmail.com>
2022-08-09 12:48:47 +0300

    create profilepage app

---
+++ profilepage/views.py
@@ -0,0 +1,18 @@
+from django.http.response import HttpResponse
+from django.template import loader
+
+from .models import ProfilePage
+from django.apps import apps
+
+import datetime
+
+
+def profile_detail(request, profilepage_id):
+    template = loader.get_template('profilepage.html')
+    profile = ProfilePage.objects.get(id=profilepage_id)
+    Post = apps.get_model('post', 'Post')
+    posts = Post.objects.filter(author=profile.user_account.user)
+    own_profile = request.user and (request.user.id == profile.user_account.user.id)
+    return HttpResponse(template.render({'profile': profile, 'posts': posts, 'own_profile': own_profile}, request))
+
+
