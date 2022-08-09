b9eb2dcd4c758777eebef81cc3809cee5a4d763a
Agnes <koinageagness@gmail.com>
2022-08-09 12:37:17 +0300

    create business app

---
+++ business/__init__.py
@@ -0,0 +1,9 @@
+""" 
+The joy of coding Python should be in seeing short,
+concise,readable classes that expressalot of action ina
+small amount of clear code-not in reams of trivial code
+that bores the reader to death.
+
+-Guido van Rossum
+
+"""
